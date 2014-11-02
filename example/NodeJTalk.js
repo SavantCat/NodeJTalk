72d6fed6eaba3437d322b10a54921cf048b7a782
Savant_Cat <scanf@me.com>
2014-11-02 21:36:17 +0900

    NodeJTalk

---
+++ example/NodeJTalk.js
@@ -0,0 +1,31 @@
+﻿var nodejtalk   = require('./NodeJTalk/nodejtalk');
+var Dictionary = "./NodeJTalk/dic";
+var Voice	   = "./NodeJTalk/voice\\mei_happy.htsvoice";
+var Sampling   = 48000;
+var file_name  = "voice.wav";
+var text = '我輩は猫である。名前はまだ無い。どこで生れたかとんと見当がつかぬ。何でも薄暗いじめじめした所でニャーニャー泣いていた事だけは記憶している。';
+
+//OpenJTalk------------------------------------------
+nodejtalk.setup(Dictionary,Voice,Sampling,file_name);
+
+nodejtalk.run(text);
+
+nodejtalk.end();
+//---------------------------------------------------
+
+//バイナリとしてBuffer変数にロードしてあれこれする準備----------
+var fs = require("fs");
+var buffer = fs.readFileSync( file_name );
+console.log("Buffer size:"+buffer.length);
+//---------------------------------------------------
+
+/*
+var sys = require('sys');
+for(var i=0;i<buffer.length;i++){
+    if (i % 15 == 0 && i>0) {
+        console.log("");
+    }
+    sys.print(buffer[i]);
+}
+*/
+
